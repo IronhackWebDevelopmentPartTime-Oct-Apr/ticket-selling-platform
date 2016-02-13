f39a70734d2ccab5172ee5347fefe5c91a89420d
Jorge Juan <hi.jorgejuanp@gmail.com>
2016-02-13 15:26:38 +0100

    Show details links point to events/event.id

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 
   $('.filter-set').hide(); //hide dropdown filter menu
 
+// ********************************
+// AJAX request
   $('.form-cities').on('submit', function() {
     event.preventDefault();
-    $('.filter-set').toggle(); //show dropdown filter menu
+    $('.filter-set').slideDown(); //show dropdown filter menu
     var API_URL = 'https://api.seatgeek.com/2/events?venue.city='
     var userInput = $('.inputCity').val();
 
@@ -16,8 +18,13 @@ $(document).ready(function(){
     }); //end AJAX
   });
 
+// ********************************
+// display request results in HTML
   function showEvents(response) {
     var events = response.events;
+    // order events by date (default order)
+    filterResults(events, date);
+
     var statusHTML = '';
 
     $.each(events, function(index, event) {
@@ -25,7 +32,7 @@ $(document).ready(function(){
         var eventId = event.id; //store event id for the event-detail-view
 
         statusHTML += '<li class="event-card">';
-          statusHTML += '<h3 class="event-title">' + '<a href="#event-detail-view">' + event.title + '</a>' + '</h3>';
+          statusHTML += '<h3 class="event-title">' + '<a href="/events/'  + event.id + '">' + event.title + '</a>' + '</h3>';
           if(event.performers[0].image) { // if event has image, display it
             statusHTML += '<img src="' + event.performers[0].image + '">';
           }
@@ -36,11 +43,27 @@ $(document).ready(function(){
           statusHTML += event.datetime_utc;
           statusHTML += event.stats.average_price;
           //create the button to show event details
-          statusHTML += '<br><button id="' + event.id + '" type="submit" class="show-details btn btn-default">Show event details</button>';
+          statusHTML += '<br><a href="/events/'  + event.id + '" class="show-details btn btn-default">Show event details</button>';
         statusHTML += '</li>';
       }
     });
    $('.list-group').html(statusHTML);
   }
+
+// ********************************
+// filter results
+function filterResults(array, order){
+  switch (order){
+    case 'date'
+      // code
+      break;
+    case 'price_asc'
+      // code
+      break;
+    case 'price_desc'
+      // code
+      break;
+  }
+}
 
 }); //end ready
