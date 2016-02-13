67edd3474bea762d7b7e53d45420e2086ee7dca4
Rafa Ramírez <raffe90@gmail.com>
2016-02-13 09:57:30 +0100

    Adds Performer model & index/show actions

--- app/models/active_api.rb
+++ app/models/active_api.rb
@@ -21,4 +21,16 @@ class ActiveApi
 
     return body
   end
+
+  def self.all
+    self.request
+  end
+
+  def self.find(id)
+    self.request(id)
+  end
+
+  def self.find_by(query_hash)
+    self.request("?#{query_hash.keys.first}=#{query_hash.values.first}")
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,16 +2,4 @@ class Event < ActiveApi
   def self.endpoint
     "events"
   end
-
-  def self.all
-    self.request
-  end
-
-  def self.find(id)
-    self.request(id)
-  end
-
-  # def self.find_by(query_hash)
-  #   self.request("?#{query_hash.keys.first}=#{query_hash.values.first}")
-  # end
 end
